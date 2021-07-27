413863f12b17d9e48f18f582fbb221b2a0d544c5
cedricrenouleau <ced241@gmail.com>
2021-07-27 17:28:17 +0200

    Sending a POST http request

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -2,7 +2,7 @@ import Card from '../ui/Card'
 import classes from './NewMeetupForm.module.css'
 import {useRef} from 'react'
 
-const NewMeetupForm = () => {
+const NewMeetupForm = (props) => {
 
     const titleInputRef = useRef()
     const imageInputRef = useRef()
@@ -24,7 +24,7 @@ const NewMeetupForm = () => {
             description: enteredDescription
         }
 
-        console.log(meetupData)
+        props.onAddMeetup(meetupData)
     }
 
     return (
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,11 +1,29 @@
+import {useHistory} from 'react-router-dom'
+
 import NewMeetupForm from '../components/meetups/NewMeetupForm'
 
 const NewMeetup = () => {
+
+    const history = useHistory()
+
+    const addMeetupHandler = (meetupData) => {
+        fetch(
+            'https://react-getting-started-2e369-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {'Content-Type': 'application/json'}
+            })
+             .then(() => {
+                history.replace('/')
+            })
+    }
+
     return (
         <section>
             <h1>Add new Meetup</h1>
 
-            <NewMeetupForm/>
+            <NewMeetupForm onAddMeetup={addMeetupHandler}/>
         </section>
     )
 }
